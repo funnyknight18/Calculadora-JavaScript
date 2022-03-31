2f518dc0457a373ed1240c37a32f84c50130be81
Joao Vitor Mocambite Lima <86741407+funnyknight18@users.noreply.github.com>
2022-03-31 00:18:34 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,38 +1,6 @@
-const numberButtons = document.querySelectorAll('button-numeros');
-const operationButtons = document.querySelectorAll('[button-operador]'); 
-const equalsButton = document.querySelectorAll('[button-valor-final]');
-const deleteButton = document.querySelectorAll('[button-deletar]');
-const allClearButton = document.querySelectorAll('[button-limpa]')
-const currentOperandTextElement = document.querySelectorAll(
-    '[data-current-operand]'
-    );
-const previusOperandTextElement = document.querySelectorAll(
-    '[data-previous-operand]'
-);
+const display = document.getElementById("data-current-operand");
+const numbers = document.querySelectorAll("[id*=number]");
+const insertNumber = (Event) =>
+  (display.textContent = Event.target.textContent);
 
-class calculator {
-     constructor(currentOperandTextElement, previusOperandTextElement) {
-         this.previusOperandTextElement = previusOperandTextElement ;
-         this.currentOperandTextElement = currentOperandTextElement ;
-
-     }
-     clear(){
-         this.currentOperandTextElement = ''; 
-         this.previusOperandTextElement = ''; 
-         this.operation = undefined ;
-     }
-     updateDisplay(){
-         this.previusOperandTextElement.innerText = this.previusOperand ; 
-         this.currentOperandTextElement.innerText = this.currentOperand ; 
-     }
-}
-
-const calculator = new calculator(
-    previusOperandTextElement, 
-    currentOperandTextElement
-); 
-
-allClearButton.addEventListener('click', () => {
-    calculator.clear(); 
-    calculator.updateDisplay(); 
-}); 
+numbers.forEach((number) => number.addEventListener("click", insertNumber));
