bc0348de3d1ce0e3ccbf05fc96b0a7b7df95b767
Joao Vitor Mocambite Lima <86741407+funnyknight18@users.noreply.github.com>
2022-04-05 01:28:39 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,3 @@
-const display = document.getElementById("data-current-operand");
-const numbers = document.querySelectorAll("[id*=number]");
-const insertNumber = (Event) =>
-  (display.textContent = Event.target.textContent);
 
-numbers.forEach((number) => number.addEventListener("click", insertNumber));
+const resultado = document.querySelector('.result'); 
+const confirmar =  document.querySelector('.igual'); 
