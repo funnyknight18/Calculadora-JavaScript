20bb5a2725f0cab5a8895843180ceb52a887dfd3
Joao Vitor Mocambite Lima <86741407+funnyknight18@users.noreply.github.com>
2022-04-06 00:31:49 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,24 @@
-
 const resultado = document.querySelector('.result'); 
 const confirmar =  document.querySelector('.igual'); 
+
+function  inserir( valor ){ 
+     resultado.innerHTML  +=  valor; 
+}
+
+function clean(){
+    resultado.innerHTML = ' '; 
+}
+
+function backSpace(){ 
+    if( resultado.textContent ){
+        let result = document.getElementById('resultado').innerHTML 
+        resultado.innerHTML = result.substring(0, result.length -1 ); 
+    }
+}
+
+
+function confirma(){
+     if( resultado.textContent != 'erro' ){
+         document.getElementById('resultado').innerHTML = eval()
+     }
+}
